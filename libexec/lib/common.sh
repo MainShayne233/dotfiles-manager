c7cc55e547ca23d85f400df3a936398d84a79a93
MainShayne233 <shaynetremblay@hotmail.com>
2018-09-23 21:07:20 -0400

    Update dotfiles put

--- libexec/lib/common.sh
+++ libexec/lib/common.sh
@@ -23,32 +23,59 @@ header() {
 }
 
 value_for_index_and_query() {
-  query=".[$1].$2"
-  path=$(echo $INDEX_JSON_FILE | jq --raw-output $query)
+  local query=".[$1].$2"
+  local path=$(echo $INDEX_JSON_FILE | jq --raw-output $query)
   echo $(eval "echo $path")
 }
 
 file_name_for_index() {
-  query="name"
+  local query="name"
   value_for_index_and_query $1 "$query"
 }
 
 system_path_for_index() {
-  query="$SYSTEM.system_path"
+  local query="$SYSTEM.system_path"
   value_for_index_and_query $1 "$query"
 }
 
 repo_path_for_index() {
-  query="$SYSTEM.repo_path"
-  rel_dir=$(value_for_index_and_query $1 "$query")
+  local query="$SYSTEM.repo_path"
+  local rel_dir=$(value_for_index_and_query $1 "$query")
   echo "$DOTFILES_DIR/$rel_dir"
 }
 
 there_is_system_configuration_for_index() {
-  query="$SYSTEM | type"
+  local query="$SYSTEM | type"
   if [[ $(value_for_index_and_query $1 $query) == 'object' ]]; then
     echo 'true'
   else
     echo 'false'
   fi
+}
+
+file_or_dir_exists() {
+  local path="$1"
+  if [[ -f $path || -d $path ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+replace_file() {
+  local new_file="$1"
+  local file_to_replace="$2"
+
+  set +e
+  if ! file_or_dir_exists $new_file; then
+    header "$new_file does not exist on system"
+  elif ! file_or_dir_exists $file_to_replace; then
+    header "$file_to_replace does not exist in repo"
+  elif diff $new_file $file_to_replace; then
+    echo "$new_file matches the repo version"
+  else
+    echo "Confirm that you want to put your version of $new_file into the repo?"
+    read -r
+  fi
+  set -e
 }
